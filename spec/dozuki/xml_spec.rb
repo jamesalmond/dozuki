5fd704c08d9e709433ecc0271c813e9363784db3
JamesAlmond <james@reservationlive.net>
2010-12-21 14:51:12 +0000

    Removed old GSI reference

--- spec/dozuki/xml_spec.rb
+++ spec/dozuki/xml_spec.rb
@@ -11,7 +11,7 @@ module Dozuki
         Nokogiri::XML.stub(:parse).and_return(nokogiri_node)
         XML::Node.stub(:new).and_return(gsi_xml_node)
       end
-      subject{GSI::XML.parse(string)}
+      subject{XML.parse(string)}
       it "should parse the xml using nokogiri" do
         Nokogiri::XML.should_receive(:parse).with(string)
         subject
