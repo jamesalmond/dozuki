ef67d1ebf54e5ebb1f5a820b05d998bf826b5e13
JamesAlmond <james@reservationlive.net>
2010-12-21 14:43:47 +0000

    Fixed C+P error

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,3 @@
 require 'rspec'
-require File.join(File.dirname(__FILE__),'../lib/gsi-xml')
+require File.join(File.dirname(__FILE__),'../lib/dozuki')
 Dir[File.join(File.dirname(__FILE__),"support/**/*.rb")].each   {|f| require f}
