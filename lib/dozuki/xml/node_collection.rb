b86ba2a63ce3f54fb5f68c3492cc3017e7c70d6d
James Almond <james@jamesalmond.com>
2010-12-31 11:08:16 +0000

    Whitespaces fixes

--- lib/dozuki/xml/node_collection.rb
+++ lib/dozuki/xml/node_collection.rb
@@ -14,13 +14,14 @@ module Dozuki
       def as_string(&blk)
         collection.each{|item| blk.call(Parser.to_string(item))}
       end
+      
       def as_int(&blk)
         collection.each{|item| blk.call(Parser.to_int(item))}
       end
+      
       def as_float(&blk)
         collection.each{|item| blk.call(Parser.to_float(item))}
       end
-      
     end
   end
 end
