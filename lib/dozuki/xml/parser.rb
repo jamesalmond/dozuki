0d7582d0611903efc8d6758026a776980ce166c6
JamesAlmond <james@reservationlive.net>
2011-04-21 11:25:55 +0100

    Whitespace cleanup

--- lib/dozuki/xml/parser.rb
+++ lib/dozuki/xml/parser.rb
@@ -1,23 +1,23 @@
 module Dozuki
   module XML
     module Parser
-    
+
       def self.to_string(node)
         node.text.strip
       end
-      
+
       def self.to_int(node)
         string = to_string(node)
         raise InvalidFormat.new("Parsing error", :node => node, :value => string, :format => "int") unless string =~ /^-?[0-9]+$/
         string.to_i
       end
-      
+
       def self.to_float(node)
         string = to_string(node)
         raise InvalidFormat.new("Parsing error", :node => node, :value => string, :format => "float") unless string =~ /^-?[0-9]+(\.[0-9]+)?$/
         string.to_f
       end
-      
+
     end
   end
 end
